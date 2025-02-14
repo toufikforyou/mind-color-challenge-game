f6fde5ac6ab37bb0571de43d05fce70a3ccfcb52
MH TOUFIK <toufik@sopnolikhi.com>
2025-02-15 03:12:39 +0600

    Add Jetpack Compose navigation and ViewModel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,11 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
