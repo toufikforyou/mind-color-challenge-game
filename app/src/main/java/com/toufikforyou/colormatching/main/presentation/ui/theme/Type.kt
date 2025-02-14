cef2b8dc1ac76fbadb6940e9ac3e424155c0853a
MH TOUFIK <toufik@sopnolikhi.com>
2025-02-15 00:34:11 +0600

    Add theme configuration with color schemes and typography

--- app/src/main/java/com/toufikforyou/colormatching/main/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/toufikforyou/colormatching/main/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.toufikforyou.colormatching.main.ui.theme
+package com.toufikforyou.colormatching.main.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.FontFamily
