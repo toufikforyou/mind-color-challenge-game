418ddd1adf9834b79015d653f3c349e9862a7812
MH TOUFIK <toufik@sopnolikhi.com>
2025-02-15 00:16:44 +0600

    initial setup

--- app/src/main/java/com/toufikforyou/colormatching/MainActivity.kt
+++ app/src/main/java/com/toufikforyou/colormatching/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.toufikforyou.colormatching.ui.theme.ColorMatchingTheme
+import com.toufikforyou.colormatching.main.ui.theme.ColorMatchingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +20,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             ColorMatchingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center,
+
+                        ) {
+                        Text(text = "Color Matching Game")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ColorMatchingTheme {
-        Greeting("Android")
     }
 }
