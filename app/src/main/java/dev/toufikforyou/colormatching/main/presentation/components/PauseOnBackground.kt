6519b677fff639416b5505a1a0dbe03a7593a650
MH TOUFIK <toufikforyou@gmail.com>
2025-07-28 11:47:59 +0600

    Fix lifecycle owner import in PauseOnBackground

--- app/src/main/java/dev/toufikforyou/colormatching/main/presentation/components/PauseOnBackground.kt
+++ app/src/main/java/dev/toufikforyou/colormatching/main/presentation/components/PauseOnBackground.kt
@@ -2,9 +2,9 @@ package dev.toufikforyou.colormatching.main.presentation.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 /**
  * A composable that observes the lifecycle and calls a callback when the app goes to background.
